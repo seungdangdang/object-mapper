0d6fd5e2600b65cbab104c43ab9186e4811f51dc
yunseung <yseungun.21@gmail.com>
2024-02-13 20:44:28 +0900

    feat: parsing, value change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,60 @@
 package org.example;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.util.Arrays;
+import java.util.List;
+import org.example.dto.Car;
+import org.example.dto.User;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         System.out.println("Hello world!");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        User user = new User();
+        user.setName("홍길동");
+        user.setAge(10);
+
+        Car car1 = new Car();
+        car1.setName("k5");
+        car1.setCarNumber("11가 11111");
+        car1.setType("sedan");
+
+        Car car2 = new Car();
+        car2.setName("Q5");
+        car2.setCarNumber("22가 11111");
+        car2.setType("SUV");
+
+        List<Car> carList = Arrays.asList(car1, car2);
+        user.setCars(carList);
+
+        System.out.println(user);
+
+        String json = objectMapper.writeValueAsString(user);
+        System.out.println(json);
+
+        // node parsing
+        JsonNode jsonNode = objectMapper.readTree(json);
+        String _name = jsonNode.get("name").asText();
+        int _age = jsonNode.get("age").asInt();
+        System.out.println(_name + ", " + _age);
+
+        JsonNode cars = jsonNode.get("cars");
+        ArrayNode arrayNode = (ArrayNode)cars;
+        List<Car> _cars = objectMapper.convertValue(arrayNode, new TypeReference<List<Car>>() {});
+        System.out.println(_cars);
+
+        // node value change
+        ObjectNode objectNode = (ObjectNode) jsonNode;
+        objectNode.put("name", "seung");
+        objectNode.put("age", 20);
+
+        System.out.println(objectNode.toPrettyString());
     }
 }
